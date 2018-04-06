3c5743d8b341ec866ded1bc2684043674b890986
Viji <viji806@gmail.com>
2018-04-05 17:16:03 -0700

    part 6 changes

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/components/Card.java
@@ -0,0 +1,11 @@
+package com.adobe.aem.guides.wknd.components;
+
+public interface Card {
+	
+	String getTitle();
+	String getDescription();
+	String getLinkPath();
+	String getImageSrc();
+	boolean isEmpty();
+
+}
